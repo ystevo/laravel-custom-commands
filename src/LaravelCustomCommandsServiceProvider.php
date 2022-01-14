2d98aea74db8484693df4960e13a4167b13947e5
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2022-01-14 16:52:16 -0300

    added run development server custom command

---
+++ src/LaravelCustomCommandsServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DayGarcia\AmazonSpApiLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCustomCommandsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+
+
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/customcommands.php' => config_path('customcommands.php'),
+        ]);
+        // check if the Console/Commands/ directory exists
+        if (!file_exists(app_path('Console/Commands/'))) {
+            mkdir(app_path('Console/Commands/'), 0755, true);
+        }
+        $this->publishes([
+            __DIR__ . '/app/Console/Commands/' => app_path('Console/Commands/'),
+        ]);
+    }
+}
---
+++ src/config/customcommands.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | All variables that you have to use in your custom commands
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | Laravel Custom Commands Docs.:
+    | https://github.com/ystevo/laravel-custom-commands
+    |
+    */
+
+    'default_port'  => env('CUSTOM_COMMANDS_DEFAULT_PORT'),
+];
